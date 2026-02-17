3af8feb622d0d98408bfacd64085ce7a9c0c3964
yxm011 <mammadovyagub@sabanciuniv.edu>
2026-02-17 22:11:31 +0300

    Update vite config to force cache busting

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
